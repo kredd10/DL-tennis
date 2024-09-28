f9cf5a69f9785280cd0c237fc4557d9b839d6cd7
Manish Reddy-CAV <mreddy10@hawk.iit.edu>
2024-09-28 17:11:08 -0500

    Update yolo_inference.py
    added for tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolo5_last.pt')
-#model = YOLO('models/yolo5_last.pt')
+#model = YOLO('models/yolo5_last.pt') #for tennis balls
+#model = YOLO('models/yolo5_last.pt') #for tennis balls
 
-result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+model = YOLO('models/yolov8x') # for tracking
+
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
 
