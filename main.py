9b94a8a50c64e7c5981326605746a1907e853ab8
Manish Reddy-CAV <mreddy10@hawk.iit.edu>
2024-09-28 17:50:39 -0500

    read & save the videos
    read and save the videos

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from utils import read_video, save_video
+
 def main():
+    input_video_path = 'input_videos/input_video.mp4'
+
+    video_frames = read_video(input_video_path)
+
+    save_video(video_frames, 'output_videos/output_video.avi')
     print("Hello, World!")
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         main()
