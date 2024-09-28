53d20702462965f08170936aa16470c66ccbbf4e
Manish Reddy-CAV <mreddy10@hawk.iit.edu>
2024-09-28 12:15:56 -0500

    Update yolo_inference.py
    updating the bounding boxes

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,4 +2,9 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-model.predict('input_videos/image.png', save=True)
+result = model.predict('input_videos/image.png', save=True)
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
+
