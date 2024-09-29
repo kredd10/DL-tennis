5a3f8d2938a3e2f18af1b922231ebf931f80576f
Manish Reddy-CAV <mreddy10@hawk.iit.edu>
2024-09-28 20:00:34 -0500

    player tracker update

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from utils import read_video, save_video
 
+from trackers import PlayerTracker
+
 def main():
+
+    #Read the video
     input_video_path = 'input_videos/input_video.mp4'
 
     video_frames = read_video(input_video_path)
+
+
+    #detecting the players
+    player_tracker = PlayerTracker(model_path='yolov8x')
+
+    player_detections = player_tracker.detect_frame(video_frames)
 
     save_video(video_frames, "output_videos/output_video.avi")
     
---
+++ trackers/player_tracker.py
@@ -0,0 +1,46 @@
+from ultralytics import YOLO
+import cv2
+
+class PlayerTracker:
+    def __init__(self):
+        self.model = YOLO('models/yolov8x') # for tracking
+
+    def detect_frames(self, frames):
+        player_detections = []
+
+        for frame in frames:
+            player_dict = self.detect_frame(frame)
+            player_detections.append(player_dict)
+
+        return player_detections
+
+    def detect_frame(self, frame):
+        results = self.model(frame, persist=True)[0]
+        id_name_dict = results.names
+
+
+
+        player_dict = {}
+        for box in results.boxes:
+            track_id = int(box.id.tolist()[0])
+            result = box.xyxy[0].tolist()[0]
+            object_cls_id = box.cls.tolist()[0]
+            object_cls_name = id_name_dict[object_cls_id]
+            if object_cls_name == 'person':
+                player_dict[track_id] = result
+
+        return player_dict 
+    
+
+    def draw_bboxes(self, video_frames, player_detections):
+        output_video_frames = []
+        for frame, player_dict in zip(video_frames, player_detections):
+            #draw the Bounding boxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2, y2 = bbox
+                cv2.putText(frame, f"Player {track_id}", (int(bbox([1] -10))), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,0, 255), 2)
+            output_video_frames.append(frame)
+
+        return output_video_frames
+        
